087ebd45e473f9fea2d4e390335c6e2bffa145d3
FujimuraShin <sfujimura2@gmail.com>
2021-12-01 22:14:32 +0900

    Index&Create

---
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Owner; //Eloquent
+use Illuminate\Support\Facades\DB; //QueryBuilder
+use Carbon\Carbon;
+
+class OwnersController extends Controller
+{
+   
+    public function index()
+    {
+        //
+        //$e_all=Owner::all();
+        //$q_get=DB::table('owners')->select('name','created_at')->get();
+       //$q_first=DB::table('owners')->select('name')->first();
+        
+        //$c_test=collect(['
+            //'name'=>'テスト'
+        //]);
+        
+        //dd($e_all,$q_get,$q_first,$c_test);
+
+        $owners=Owner::select('name','email','created_at')->get();
+
+        return view('owners.index',compact('owners'));
+        
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('owners.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/owners/index.blade.php
@@ -0,0 +1,60 @@
+{{--
+001_エロクアント
+    @foreach($e_all as $e_owner)
+        {{ $e_owner-> name }}               
+        {{ $e_owner->created_at->diffForHumans() }}
+    @endforeach
+<br/>
+
+<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+<script src="{{ asset('js/app.js') }}"></script>
+
+ 002_クエリビルダ
+     @foreach($q_get as $q_owner)
+        {{ $q_owner->name }}              
+        {{ $q_owner->created_at }}
+     @endforeach
+--}}
+
+    <x-guest-layout>
+   
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            オーナ一覧
+        </h2>
+    
+
+<section class="text-gray-600 body-font">
+<div class="container px-5　mx-auto">
+<div class="flex justify-end mb-4" >
+  <button onclick="location.href='{{route('owners.create')}}'" class=" text-white bg-pink-500 border-0 py-2 px-8 focus:outline-none hover:bg-pink-600 rounded text-lg">新規登録する</button>
+</div>    
+    <div class="lg:w-2/3 w-full mx-auto overflow-auto">
+      <table class="table-auto w-full text-left whitespace-no-wrap">
+        <thead>
+          <tr>
+            <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">名前</th>
+            <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">メールアドレス</th>
+            <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">制作日</th>
+            <th class="w-10 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br"></th>
+          </tr>
+        </thead>
+        <tbody>
+        @foreach($owners as $owner)
+          <tr>
+            <td class="px-4 py-3">{{ $owner-> name }}  </td>
+            <td class="px-4 py-3">{{ $owner-> email }}  </td>(
+            <td class="px-4 py-3">{{ $owner-> created_at->diffForHumans() }}  </td>
+            <td class="w-10 text-center">
+              <input name="plan" type="radio">
+            </td>
+          </tr>
+        @endforeach 
+        </tbody>
+      </table>
+    </div>
+  </div>
+</section>
+  
+</x-guest-layout>
+
+  
