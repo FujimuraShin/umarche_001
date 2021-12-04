9b74efd72a0bf52ff6abd466f8b4ba7d9d01bf66
FujimuraShin <sfujimura2@gmail.com>
2021-12-04 22:41:23 +0900

    ShopImage

--- resources/views/shops/index.blade.php
+++ resources/views/shops/index.blade.php
@@ -1,14 +1,48 @@
 
 
+    <x-guest-layout>
+   
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            店舗情報
+        </h2>
+
+<section class="text-gray-600 body-font">
+<div class="container px-5　mx-auto">
   
+    <div class="lg:w-2/3 0w-full mx-auto overflow-auto">
+      
+    @foreach($shops as $shop)
+                  <div class="w-1/2 p-4">
+                    <a href="{{route('shops.edit',['shop'=>$shop->id])}}">
+                      <div class="border rounded-md p-4">
+                        <div class="md-4">
+                          @if($shop->is_selling)
+                            <span class="border p-2 rounded-md bg-blue-400 text-white">販売中</span>
+                          @else
+                          <span class="border p-2 rounded-md bg-red-400 text-white">販売停止中</span>
+                         
+                          @endif
+                        </div>
+                          <br/>
+                          <div class="text-xl">{{ $shop->name }}</div>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    店舗情報
+                          <div>
+                            @if(empty($shop->filename))
+                              <img src="{{asset('images/noimage.png')}}">
+                            @else
+                              <img src="{{asset('sotrage/shops/'.$shop->filename)}}">
+                            @endif
+                          </div>
+                    </a>
                 </div>
-            </div>
-        </div>
-    </div>
+                  @endforeach
 
+      
+    </div>
+  </div>
+</section>
+
+
+</x-guest-layout>
+
+  
